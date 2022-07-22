cd107b4e733c3be6aaf45edf18b31304709ba2b7
hackedlings <pks9862728888@gmail.com>
2022-07-22 22:44:05 +0530

    first commit

---
+++ src/main/java/com/demo/multidbconnectionspringbootdemo/MultiDbConnectionSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.multidbconnectionspringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiDbConnectionSpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiDbConnectionSpringBootDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/multidbconnectionspringbootdemo/MultiDbConnectionSpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.multidbconnectionspringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiDbConnectionSpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
